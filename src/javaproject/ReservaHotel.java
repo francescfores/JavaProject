289c9c3ebb75304b3673187695ab079d7624735f
francesc <ffores93@gmail.com>
2015-10-28 17:38:00 +0100

    JFrame menu inicial

--- src/javaproject/ReservaHotel.java
+++ src/javaproject/ReservaHotel.java
@@ -5,9 +5,23 @@
  */
 package javaproject;
 
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author sergi
+ * @author forés
+ * 
  */
 public class ReservaHotel {
 
